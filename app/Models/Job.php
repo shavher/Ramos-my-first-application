e65a43963d06014227f15ee55039c4bb1195c19f
ex4gch <toounit@gmail.com>
2025-09-21 23:03:34 +0800

    Chapter 3: Connect to database with Migrations and Eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,33 +1,11 @@
 <?php
 namespace App\Models;
-class Job
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class Job extends Model
 {
-public static function all()
-{
-return [
-[
-'id' => 1,
-'title' => 'Director',
-'salary' => '$50,000'
-],
-[
-'id' => 2,
-'title' => 'Programmer',
-'salary' => '$10,000'
-],
-[
-'id' => 3,
-'title' => 'Teacher',
-'salary' => '$40,000'
-],
-];
-}
-public static function find($id)
-{
-$job = \Illuminate\Support\Arr::first(static::all(), fn($job) => $job['id'] == $id);
-if (! $job) {
-abort(404);
-}
-return $job;
-}
+use HasFactory;
+// By convention, Laravel assumes a 'jobs' table.
+// We need to tell it to use our 'job_listings' table instead.
+protected $table = 'job_listings';
 }
